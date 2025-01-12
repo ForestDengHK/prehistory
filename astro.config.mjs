8acbd6a6e2827a86f2104f5001cc9a3de784c0c1
Forest Deng <forest.jinying.denghk@outlook.com>
2025-01-12 17:35:53 +0000

    vercel deployment fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,8 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://localhost:4321',
-  base: '/',
   output: 'server',
-  build: {
-    format: 'directory'
-  },
   adapter: node({
     mode: 'standalone'
-  }),
-  server: {
-    port: 4321,
-  },
+  })
 });
