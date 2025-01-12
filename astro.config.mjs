eabd57092c00a9559ee57f64c14327f5c354f716
Forest Deng <forest.jinying.denghk@outlook.com>
2025-01-12 17:40:32 +0000

    vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel()
+  adapter: vercel({
+    analytics: true,
+    maxDuration: 60,
+    runtime: 'nodejs18.x'
+  })
 });
