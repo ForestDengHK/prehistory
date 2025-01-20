1b83a71390208c0d6b76f4c5bf25241ddba41403
Forest Deng <forest.jinying.denghk@outlook.com>
2025-01-20 08:48:50 +0000

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.xinhao.eu',
   base: '/',
   output: 'server',
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/admin/'),
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
   build: {
     format: 'directory'
   },
